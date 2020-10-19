145880d2702703848546b41878d663232b5c3e41
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-10-19 14:36:53 +1100

    Create post

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 9999;
+const port = process.env.PORT || 9991;
+require("dotenv").config();
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
-.then(() => { console.log("Connected to MongoDB!"); })
-.catch(err => { console.error("Error connecting to mongoDB", err); });
-
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongoDB", err);
+  });
+app.use(express.json());
 //post routes
 const postRouter = require("./routes/post_routes.js");
 app.use("/", postRouter);
 
 app.listen(port, () => {
-    console.log( `Express app listening on port ${port}`);
-    
+  console.log(`Express app listening on port ${port}`);
 });
